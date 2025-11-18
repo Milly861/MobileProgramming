f27a733ad963d61df621900d2dc97978d0cfbad1
Milly861 <plmrv2000@gmail.com>
2025-11-18 22:00:23 +0300

    added resizes and alignments

--- Unit2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
@@ -16,15 +18,9 @@ import com.example.diceroller.ui.theme.DiceRollerTheme
 class MainActiviy : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             DiceRollerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DiceRollerApp()
             }
         }
     }
@@ -33,10 +29,11 @@ class MainActiviy : ComponentActivity() {
 @Preview
 @Composable
 fun DiceRollerApp() {
-
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
 }
 
 @Composable
-fun DiceWithButtonAndImage() {
-
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 }
