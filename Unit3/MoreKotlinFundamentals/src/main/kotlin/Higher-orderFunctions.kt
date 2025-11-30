fdb8b6a14f46a40036095514117047c38aeba0c0
Milly <plmrv2000@gmail.com>
2025-11-30 17:51:35 +0300

    using map()

--- Unit3/MoreKotlinFundamentals/src/main/kotlin/Higher-orderFunctions.kt
+++ Unit3/MoreKotlinFundamentals/src/main/kotlin/Higher-orderFunctions.kt
@@ -53,7 +53,11 @@ val cookies = listOf(
 )
 
 fun main() {
-    cookies.forEach {
-        println("Menu item: ${it.name}")
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
+    }
+    println("Full menu:")
+    fullMenu.forEach {
+        println(it)
     }
 }
