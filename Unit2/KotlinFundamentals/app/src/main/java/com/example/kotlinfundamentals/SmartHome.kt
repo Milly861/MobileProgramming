3fb6fa7d378faa9b5efde89aab59d1bacb65a224
Milly861 <plmrv2000@gmail.com>
2025-11-30 10:00:35 +0300

    define SmartLightDevice

--- Unit2/KotlinFundamentals/app/src/main/java/com/example/kotlinfundamentals/SmartHome.kt
+++ Unit2/KotlinFundamentals/app/src/main/java/com/example/kotlinfundamentals/SmartHome.kt
@@ -47,6 +47,22 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
     }
 }
 
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel.")
+    }
+}
+
 fun main() {
     val smartTvDevice = SmartDevice()
     println("Device name is: ${smartTvDevice.name}")
