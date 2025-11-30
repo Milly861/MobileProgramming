7965f823188fe29b63fcc15d9d450f19dc9d5b21
Milly861 <plmrv2000@gmail.com>
2025-11-30 17:28:13 +0300

    finish Quiz

--- Unit3/MoreKotlinFundamentals/src/main/kotlin/Quiz.kt
+++ Unit3/MoreKotlinFundamentals/src/main/kotlin/Quiz.kt
@@ -1,38 +1,31 @@
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
 data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
 )
 
-enum class Difficulty {
-    EASY, MEDIUM, HARD
-}
-
-interface ProgressPrintable {
-    val progressText: String
-    fun printProgressBar()
-}
-
 class Quiz : ProgressPrintable {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-
     override val progressText: String
-        get() = "${answered} of ${total} answered"
-
-    companion object StudentProgress {
-        var total: Int = 10
-        var answered: Int = 3
-    }
+        get() = "$answered of $total answered"
 
     override fun printProgressBar() {
-        repeat(Quiz.answered) { print("▓") }
-        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        repeat(answered) { print("▓") }
+        repeat(total - answered) { print("▒") }
         println()
         println(progressText)
     }
-
+    val question1 = Question("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question("How many days are there between full moons?", 28, Difficulty.HARD)
     fun printQuiz() {
         question1.let {
             println(it.questionText)
@@ -53,10 +46,16 @@ class Quiz : ProgressPrintable {
         }
         println()
     }
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
 }
 
 fun main() {
     Quiz().apply {
         printQuiz()
+        printProgressBar()
     }
 }
