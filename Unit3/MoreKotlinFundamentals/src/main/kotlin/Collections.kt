76ff94647942e8c5e06c3dbcec1374e4ee8bd504
Milly861 <plmrv2000@gmail.com>
2025-11-30 17:41:38 +0300

    testing mutableSetOf

--- Unit3/MoreKotlinFundamentals/src/main/kotlin/Collections.kt
+++ Unit3/MoreKotlinFundamentals/src/main/kotlin/Collections.kt
@@ -2,22 +2,15 @@ package main.kotlin
 
 
 fun main() {
-    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
     println(solarSystem.size)
-    println(solarSystem[2])
-    println(solarSystem.get(3))
-    println(solarSystem.indexOf("Earth"))
-    println(solarSystem.indexOf("Pluto"))
-    for (planet in solarSystem) {
-        println(planet)
-    }
     solarSystem.add("Pluto")
-    solarSystem.add(3, "Theia")
-    solarSystem[3] = "Future Moon"
-    println(solarSystem[3])
-    println(solarSystem[9])
-    solarSystem.removeAt(9)
-    solarSystem.remove("Future Moon")
+    println(solarSystem.size)
     println(solarSystem.contains("Pluto"))
-    println("Future Moon" in solarSystem)
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
+
 }
