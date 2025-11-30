1388f310f74f4e30d90b7421233fc83dade76de5
Milly861 <plmrv2000@gmail.com>
2025-11-30 18:08:08 +0300

    Second task completed

--- Unit3/MoreKotlinFundamentals/src/main/kotlin/ClassesAndCollections.kt
+++ Unit3/MoreKotlinFundamentals/src/main/kotlin/ClassesAndCollections.kt
@@ -1,19 +1,19 @@
-package main.kotlin
+enum class Daypart {
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
 
 data class Event(
     val title: String,
     val description: String?,
-    val daypart: String,
+    val daypart: Daypart,
     val durationInMinutes: Int
 )
 
 val studyEvent = Event(
     title = "Study Kotlin",
     description = "Commit to studying Kotlin at least 15 minutes per day.",
-    daypart = "Evening",
+    daypart = Daypart.EVENING,
     durationInMinutes = 15
 )
-
-fun main() {
-    println(studyEvent)
-}
