bf71fcd333e49285888b37b039c83ee7a1a45e8c
Milly861 <plmrv2000@gmail.com>
2025-11-30 10:02:40 +0300

    define SmartHome and functions

--- Unit2/KotlinFundamentals/app/src/main/java/com/example/kotlinfundamentals/SmartHome.kt
+++ Unit2/KotlinFundamentals/app/src/main/java/com/example/kotlinfundamentals/SmartHome.kt
@@ -63,6 +63,44 @@ class SmartLightDevice(deviceName: String, deviceCategory: String) :
     }
 }
 
+class SmartHome(
+    val smartTvDevice: SmartTvDevice,
+    val smartLightDevice: SmartLightDevice
+){
+    fun turnOnTv() {
+        smartTvDevice.turnOn()
+    }
+
+    fun turnOffTv() {
+        smartTvDevice.turnOff()
+    }
+
+    fun increaseTvVolume() {
+        smartTvDevice.increaseSpeakerVolume()
+    }
+
+    fun changeTvChannelToNext() {
+        smartTvDevice.nextChannel()
+    }
+
+    fun turnOnLight() {
+        smartLightDevice.turnOn()
+    }
+
+    fun turnOffLight() {
+        smartLightDevice.turnOff()
+    }
+
+    fun increaseLightBrightness() {
+        smartLightDevice.increaseBrightness()
+    }
+
+    fun turnOffAllDevices() {
+        turnOffTv()
+        turnOffLight()
+    }
+}
+
 fun main() {
     val smartTvDevice = SmartDevice()
     println("Device name is: ${smartTvDevice.name}")
