4a6010592d1924216447554ad9d2061154525c5c
Milly861 <plmrv2000@gmail.com>
2025-11-30 13:42:57 +0300

    added class Quiz

--- Unit3/MoreKotlinFundamentals/src/main/kotlin/Quiz.kt
+++ Unit3/MoreKotlinFundamentals/src/main/kotlin/Quiz.kt
@@ -1,4 +1,4 @@
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
@@ -8,8 +8,18 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-fun main() {
+class Quiz {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+
+fun main() {
+
 }
