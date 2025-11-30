b160faac7b0b6ffc0b62fffdf663224f3cce26ed
Milly <plmrv2000@gmail.com>
2025-11-30 17:56:59 +0300

    using groupBy

--- Unit3/MoreKotlinFundamentals/src/main/kotlin/Higher-orderFunctions.kt
+++ Unit3/MoreKotlinFundamentals/src/main/kotlin/Higher-orderFunctions.kt
@@ -61,12 +61,15 @@ fun main() {
         println(it)
     }
 
-    val softBakedMenu = cookies.filter {
-        it.softBaked
-    }
-
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu = groupedMenu[true] ?: listOf()
+    val crunchyMenu = groupedMenu[false] ?: listOf()
     println("Soft cookies:")
     softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
         println("${it.name} - $${it.price}")
     }
 }
