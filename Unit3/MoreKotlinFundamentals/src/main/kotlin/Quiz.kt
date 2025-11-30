5c31d6f132c389837125e40ad61844aa366747cc
Milly861 <plmrv2000@gmail.com>
2025-11-30 17:03:43 +0300

    added interface

--- Unit3/MoreKotlinFundamentals/src/main/kotlin/Quiz.kt
+++ Unit3/MoreKotlinFundamentals/src/main/kotlin/Quiz.kt
@@ -1,4 +1,3 @@
-
 data class Question<T>(
     val questionText: String,
     val answer: T,
@@ -9,27 +8,32 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
 
     companion object StudentProgress {
         var total: Int = 10
         var answered: Int = 3
     }
-}
 
-val Quiz.StudentProgress.progressText: String
-    get() = "${answered} of ${total} answered"
-
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.answered) { print("▓") }
-    repeat(Quiz.total - Quiz.answered) { print("▒") }
-    println()
-    println(Quiz.progressText)
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
 }
 
 fun main() {
-    Quiz.printProgressBar()
+    Quiz().printProgressBar()
 }
