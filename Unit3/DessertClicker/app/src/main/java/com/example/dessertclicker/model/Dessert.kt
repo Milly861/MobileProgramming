931299d9075cd65ed61ca2b9bb45724e30ac759c
Milly861 <plmrv2000@gmail.com>
2025-12-09 21:43:21 +0300

    used starter code

--- Unit3/DessertClicker/app/src/main/java/com/example/dessertclicker/model/Dessert.kt
+++ Unit3/DessertClicker/app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -14,10 +14,9 @@
  * limitations under the License.
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-}
+package com.example.dessertclicker.model
+
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
