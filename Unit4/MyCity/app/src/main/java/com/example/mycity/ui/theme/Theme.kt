21d807681798d9d7f4d7cc89e0dae8079b5ecd2f
Milly861 <plmrv2000@gmail.com>
2025-12-16 22:05:42 +0300

    change ui/theme/ and added fonts

--- Unit4/MyCity/app/src/main/java/com/example/mycity/ui/theme/Theme.kt
+++ Unit4/MyCity/app/src/main/java/com/example/mycity/ui/theme/Theme.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:Unit4/MyCity/app/src/main/java/com/example/mycity/ui/theme/Theme.kt
 package com.example.mycity.ui.theme
-========
-package com.example.grid.ui.theme
->>>>>>>> 9fb48fe3418ad2ccbdb96835d0cc3c5c75469376:Unit3/Grid/app/src/main/java/com/example/grid/ui/theme/Theme.kt
 
 import android.app.Activity
 import android.os.Build
@@ -19,24 +15,73 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = primary_dark,
-    secondary = secondary_dark,
-    tertiary = tertiary_dark
+private val lightColors = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    secondaryContainer = md_theme_light_secondaryContainer,
+    onSecondaryContainer = md_theme_light_onSecondaryContainer,
+    tertiary = md_theme_light_tertiary,
+    onTertiary = md_theme_light_onTertiary,
+    tertiaryContainer = md_theme_light_tertiaryContainer,
+    onTertiaryContainer = md_theme_light_onTertiaryContainer,
+    error = md_theme_light_error,
+    errorContainer = md_theme_light_errorContainer,
+    onError = md_theme_light_onError,
+    onErrorContainer = md_theme_light_onErrorContainer,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+    surfaceVariant = md_theme_light_surfaceVariant,
+    onSurfaceVariant = md_theme_light_onSurfaceVariant,
+    outline = md_theme_light_outline,
+    inverseOnSurface = md_theme_light_inverseOnSurface,
+    inverseSurface = md_theme_light_inverseSurface,
+    inversePrimary = md_theme_light_inversePrimary,
+    surfaceTint = md_theme_light_surfaceTint,
+    outlineVariant = md_theme_light_outlineVariant,
+    scrim = md_theme_light_scrim,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = primary_light,
-    secondary = secondary_light,
-    tertiary = tertiary_light
+
+private val darkColors = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    secondaryContainer = md_theme_dark_secondaryContainer,
+    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+    tertiary = md_theme_dark_tertiary,
+    onTertiary = md_theme_dark_onTertiary,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+    error = md_theme_dark_error,
+    errorContainer = md_theme_dark_errorContainer,
+    onError = md_theme_dark_onError,
+    onErrorContainer = md_theme_dark_onErrorContainer,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_surfaceVariant,
+    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+    outline = md_theme_dark_outline,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
+    inverseSurface = md_theme_dark_inverseSurface,
+    inversePrimary = md_theme_dark_inversePrimary,
+    surfaceTint = md_theme_dark_surfaceTint,
+    outlineVariant = md_theme_dark_outlineVariant,
+    scrim = md_theme_dark_scrim,
 )
 
 @Composable
-<<<<<<<< HEAD:Unit4/MyCity/app/src/main/java/com/example/mycity/ui/theme/Theme.kt
 fun MyCityTheme(
-========
-fun GridTheme(
->>>>>>>> 9fb48fe3418ad2ccbdb96835d0cc3c5c75469376:Unit3/Grid/app/src/main/java/com/example/grid/ui/theme/Theme.kt
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
@@ -48,8 +93,8 @@ fun GridTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkColors
+        else -> lightColors
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
@@ -65,4 +110,5 @@ fun GridTheme(
         typography = Typography,
         content = content
     )
+
 }
--- Unit4/MyCity/app/src/main/java/com/example/mycity/ui/theme/Type.kt
+++ Unit4/MyCity/app/src/main/java/com/example/mycity/ui/theme/Type.kt
@@ -2,33 +2,51 @@ package com.example.mycity.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.mycity.R
+
+val JosefinSans = FontFamily(
+    Font(R.font.josefin_sans_semibold, FontWeight.SemiBold),
+    Font(R.font.josefin_sans_medium, FontWeight.Medium)
+)
+val Gabarito= FontFamily(
+    Font(R.font.gabarito_regular),
+    Font(R.font.gabarito_medium, FontWeight.Medium)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Gabarito,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontFamily = JosefinSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 32.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+
+    titleMedium = TextStyle(
+        fontFamily = JosefinSans,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Gabarito,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
